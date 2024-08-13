57cb1e142fa0fd75d8a189f9715d2c61b2b5928d
Nico Cabral <ncabral010694@gmail.com>
2024-08-13 20:34:39 +0800

    intialize

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+
+import './globals.css';
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+export const metadata: Metadata = {
+  title: 'Batangas State University - RGO',
+  description: ''
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={cn(
+          "flex min-h-screen w-full flex-col bg-background font-sans antialiased",
+          fontSans.variable
+        )}>
+        {children}
+      </body>
+ 
+    </html>
+  );
+}
